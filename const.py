0649310cbfe0a612e10a612398fff8e94b2586f3
Tomás L <37879596+Tomz295@users.noreply.github.com>
2023-05-25 11:50:18 -0400

    added timedelta to const

--- const.py
+++ const.py
@@ -1,8 +1,9 @@
 """Constants for the UR10 for home assistant integration."""
 from __future__ import annotations
 import logging
+from datetime import timedelta
 
 
 DOMAIN = "urha"
 LOGGER = logging.getLogger(__package__)
-SCAN_INTERVAL = 10
+SCAN_INTERVAL = timedelta(seconds=5)
