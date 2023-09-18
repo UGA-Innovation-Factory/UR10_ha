08e105b5f9d018943412d79faa9bcf952b9671ea
Tomás L <37879596+Tomz295@users.noreply.github.com>
2023-09-18 17:23:16 -0400

    changed update rate to 1 second

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         hass.data[DOMAIN] = {"retrying_setup": True}
         await async_setup(hass, config)
 
-    ur10conn = UR10Listener("172.22.114.160", 10)
+    ur10conn = UR10Listener("172.22.114.160", 1)
     coordinator = UR10Coordinator(hass, ur10conn)
 
     try:
